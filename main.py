fb0cd04f7418aeb61d6f51d54c0b83e3d5dcf9f0
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-03 19:15:55 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import sympy as sy
+
+# I have defined the symbols for you to use in your expressions
+x, y  = sy.symbols('x y')
+# You need to use SymPy to set px equal to P(X=x)
+px = 
+# You need to use SymPy to set py equal to P(Y=y)
+py = 
+# You need to use SymPy to set ex equal to E(X)
+ex = 
+print("E(X)=", ex )
+# You need to use SymPy to set ey equal to E(Y)
+ey = 
+print("E(Y)=", ey ) 
+# You need to use SymPy to set varx equal to var(X)
+varx = 
+print("var(X)=", varx )
+# You need to use SymPy to set vary equal to var(Y)
+vary = 
+print("var(Y)=", vary )
+# You need to  use SymPy to set cov equal to cov(X,Y) 
+cov = 
+print("cov(X,Y)=", cov ) 
+# And last but not least use SymPy calculate and print the value of the correlation coefficient 
+rho = 
+print("rho(X,Y)", rho )
